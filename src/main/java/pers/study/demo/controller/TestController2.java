41857b0a8923f0577eeb9cb981245a92e8c4ee22
Leo Young <463056193@qq.com>
2021-12-05 12:12:40 +0800

    fix:测试 --user=leo

---
+++ src/main/java/pers/study/demo/controller/TestController2.java
@@ -0,0 +1,23 @@
+package pers.study.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pers.study.demo.bean.User;
+
+@Controller
+public class TestController2 {
+
+    @RequestMapping("/index")
+    public String sayHello(){
+        return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String toHello(){
+        User leo = new User("leo", 21);
+        return leo.toString();
+    }
+
+}
