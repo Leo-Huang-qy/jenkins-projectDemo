2e58eb710a196e6825dd0cc92b0b3a7867a0e4e9
Leo Young <463056193@qq.com>
2022-01-03 21:37:51 +0800

    fix:测试 --user=leo

--- src/main/java/pers/study/demo/DemoApplication.java
+++ src/main/java/pers/study/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package pers.study.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import pers.study.demo.bean.User;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        // 1. 返回IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(DemoApplication.class, args);
+
+        // 2. 查看容器里面的组件
+        String[] beanDefinitionNames = run.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
+
+        // 3. 从容器中获取组件
+        run.getBean("user01", User.class);
     }
 
 }
